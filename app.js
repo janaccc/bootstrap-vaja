87bfd32bb52d678723da82ec977501b2fab6eb24
janaccc <jantopler25@gmail.com>
2026-03-17 08:25:29 +0100

    bootstrap

---
+++ app.js
@@ -0,0 +1,17 @@
+﻿(() => {
+  const forms = document.querySelectorAll('.needs-validation');
+
+  Array.from(forms).forEach((form) => {
+    form.addEventListener('submit', (event) => {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      } else {
+        event.preventDefault();
+        alert('Hvala! Rezervacijo smo prejeli.');
+        form.reset();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  });
+})();
